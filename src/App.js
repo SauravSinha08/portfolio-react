f0d8c347f71154c10651a3c224456c443361b341
Saurav Sinha <107345317+SauravSinha08@users.noreply.github.com>
2022-09-27 00:24:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const[show, setShow] = useState(true)
+
   return (
     <div className="App">
-      <h1>Saurav's Portfolio</h1>
+      {show? <h1>Saurav's Portfolio</h1>:null}
+      <button onClick={() => {setShow(!show)}}>Toggle</button>
     </div>
   );
 }
